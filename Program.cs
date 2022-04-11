8fef51330faef6e2bc2416a4482cdf5944ce9891
FW960 <artuninp@gmail.com>
2022-04-12 01:24:12 +0800

    Added a project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson_2._8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
